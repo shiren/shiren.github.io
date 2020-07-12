fd30f65c296617160dea0393abecb2d31e39293b
shiren <shirenbeat@gmail.com>
2020-07-12 13:23:22 +0900

    Layout responsive

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,11 +1,11 @@
-import React from "react"
-import styled from "styled-components"
+import React from 'react';
+import styled from 'styled-components';
 
-import "normalize.css"
-import "../../assets/style.css"
+import 'normalize.css';
+import '../../assets/style.css';
 
-import Top from "./top"
-import Footer from "./footer"
+import Top from './top';
+import Footer from './footer';
 
 const Layout: React.FC = ({ children }) => {
   return (
@@ -22,6 +22,12 @@ const Layout: React.FC = ({ children }) => {
 const Container = styled.div`
   margin: 0 auto;
   width: 750px;
+  padding: 0 15px;
+  box-sizing: border-box;
+
+  @media only screen and (max-width: 768px) {
+    width: 100%;
+  }
 `;
 
 export default Layout;
