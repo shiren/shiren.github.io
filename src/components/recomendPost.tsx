60258f7ad648098911c1ae8e788b190c610f1b70
shiren <shirenbeat@gmail.com>
2020-09-28 22:13:51 +0900

    fix: recomendPost style bug

--- src/components/recomendPost.tsx
+++ src/components/recomendPost.tsx
@@ -1,8 +1,8 @@
-import React from "react";
-import { Link } from "gatsby";
-import styled from "styled-components";
+import React from 'react';
+import { Link } from 'gatsby';
+import styled from 'styled-components';
 
-import { Category } from "../bases/category";
+import { Category } from '../bases/category';
 
 type Props = {
   posts: Array<{
