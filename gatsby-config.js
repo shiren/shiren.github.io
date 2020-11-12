bc5737fb0423ddb31ccfdd99592c5257ecf42315
shiren <shirenbeat@gmail.com>
2020-11-12 23:20:00 +0900

    fix: restore ga

--- gatsby-config.js
+++ gatsby-config.js
@@ -125,6 +125,14 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-59192630-1',
+        pageTransitionDelay: 0,
+        head: false,
+      },
+    },
+    {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
